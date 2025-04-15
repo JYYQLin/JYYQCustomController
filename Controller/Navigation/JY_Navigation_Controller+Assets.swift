9e847a44a98cce5694e3e9b520bc23df4d1eac09
JYYQLin <92315746+JYYQLin@users.noreply.github.com>
2025-04-15 15:09:55 +0800

    JYYQLin
    修改修饰词

--- Controller/Navigation/JY_Navigation_Controller+Assets.swift
+++ Controller/Navigation/JY_Navigation_Controller+Assets.swift
@@ -11,35 +11,35 @@ extension JY_Navigation_Controller {
     /**
         如果使用默认配置, 可以通过直接添加同名素材快速替换
      */
-    open static func yq_normal_back_imageName() -> String {
+    public static func yq_normal_back_imageName() -> String {
         return "yq_icon_back_black"
     }
     /**
         如果使用默认配置, 可以通过直接添加同名素材快速替换
      */
-    open static func yq_night_back_imageName() -> String {
+    public static func yq_night_back_imageName() -> String {
         return "yq_icon_back_night"
     }
     /**
         如果使用默认配置, 可以通过直接添加同名素材快速替换
      */
-    open static func yq_white_back_imageName() -> String {
+    public static func yq_white_back_imageName() -> String {
         return "yq_icon_back_white"
     }
     
-    open static func yq_title_font() -> UIFont {
+    public static func yq_title_font() -> UIFont {
         return UIFont.systemFont(ofSize: 16)
     }
     /**
         如果使用默认配置, 可以通过直接添加同名素材快速替换
      */
-    open static func yq_title_light_textColor() -> UIColor {
+    public static func yq_title_light_textColor() -> UIColor {
         return UIColor(named: "yq_title_light_textColor") ?? UIColor(red: 33 / 255.0, green: 33 / 255.0, blue: 33 / 255.0, alpha: 1)
     }
     /**
         如果使用默认配置, 可以通过直接添加同名素材快速替换
      */
-    open static func yq_title_dark_textColor() -> UIColor {
+    public static func yq_title_dark_textColor() -> UIColor {
         return UIColor(named: "yq_title_dark_textColor") ??  UIColor(red: 249 / 255.0, green: 243 / 255.0, blue: 247 / 255.0, alpha: 0.95)
     }
 }
